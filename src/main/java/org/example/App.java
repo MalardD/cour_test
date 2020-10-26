1d6a0bd926460f8597839fd9b011561250e768b6
Malard davy <davy.malard@epsi.fr>
2020-10-26 11:17:52 +0100

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    int num = 24;
+    int num2 = 4;
+    int result = num * num2;
+    //return result;
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello world" );
+    }
+}
